19ebec3a1ce83c665c4af4473e6b70861f2ba4e7
Darryl Quinn <dman776@gmail.com>
2014-05-28 16:02:40 +0100

    updates

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='PySky',
+      version='0.1',
+      description='Python Skytap Library',
+      url='http://github.com/dman776/pysky',
+      author='Darryl Quinn',
+      author_email='dmamn776@gmail.com',
+      license='MIT',
+      packages=['pysky'],
+      zip_safe=False)
